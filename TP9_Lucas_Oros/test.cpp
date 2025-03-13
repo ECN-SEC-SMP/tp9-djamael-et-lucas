5988d474ab4882227d80b17f3aec3708f98b5823
Lucas_Oros <oroslucas1604@gmail.com>
2025-03-13 10:21:51 +0100

    push test

---
+++ TP9_Lucas_Oros/test.cpp
@@ -0,0 +1,16 @@
+//#include <stdio.h>
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <math.h>   
+#include <map>
+#include <vector>
+#include <algorithm>
+#include "test.h"
+
+
+using namespace std;
+
+void print_word(){
+  cout << "Hello World" << endl;
+}
